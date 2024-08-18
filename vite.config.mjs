6cc5dca2e9aeecf434bbb0d5ea32fb2a49cca625
shizzic <kotcich@mail.ru>
2024-08-18 14:31:34 +0300

    added nginx.conf

--- vite.config.mjs
+++ vite.config.mjs
@@ -13,4 +13,9 @@ export default defineConfig({
       '@' : path.resolve(__dirname, './src')
     }
   },
+  build: {
+    rollupOptions: {
+      external: new Regexp('/external/.*')
+    },
+  }
 })
