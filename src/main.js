de906e7d688f8fe09ab27b32592eab64f27ed098
shizzic <kotcich@mail.ru>
2024-08-24 12:11:28 +0300

    added test line

--- src/main.js
+++ src/main.js
@@ -36,6 +36,8 @@ if (curruent_db_version && curruent_db_version != +store_version) {
 }
 localStorage.setItem("store_version", store_version)
 
+const test = import.meta.env.VITE_SERVER_DOMAIN || "__SERVER_DOMAIN__"
+console.log(test)
 app.config.globalProperties.$app_name = name[0].toUpperCase() + name.slice(1)
 app.config.globalProperties.$domain = (import.meta.env.VITE_SERVER_DOMAIN || "__SERVER_DOMAIN__")
 app.config.globalProperties.$domainName = (import.meta.env.VITE_SERVER_DOMAIN_NAME || "__SERVER_DOMAIN_NAME__")
